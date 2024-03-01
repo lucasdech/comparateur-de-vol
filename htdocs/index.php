855600b2adfbe433e17d6dd4ee91de1ce46522b6
lucas dechavanne <lucasdechavanne22@gmail.com>
2024-03-01 16:45:59 +0100

    push vendredi soir

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,8 +1,44 @@
-
 <?php include "./partials/header.php";?>
 
+<!-- LIEN VERS LA FEIULLE DE STYLE -->
+
+<link rel="stylesheet" href="style/index.css">
 
 
+    <section class="presentation rounded text-white">
+        <div class="container p-5">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident velit placeat ex. Assumenda quidem omnis rem esse accusantium dolor vel obcaecati error, veniam dolorem est. Aspernatur blanditiis maiores fuga esse, nemo quis obcaecati sit, veniam incidunt voluptatibus corporis a ab et repudiandae autem pariatur officia! Odit eius voluptates dolore magnam?</p>
+        </div>
+    </section>
+
+
+    <section class="destination text-white d-flex align-items-center justify-content-around col-12">
+        <div class="col-3">
+            <p>destination</p>
+        </div>   
+        <div class="col-5"> 
+            <p>Voir les offres<i class="fa-solid fa-angles-right ms-3" style="color: #ffffff;"></i></p> 
+            
+        </div>
+    </section>
+    <section class="destination text-white d-flex align-items-center justify-content-around col-12">
+        <div class="col-3">
+            <p>destination</p>
+        </div>   
+        <div class="col-5"> 
+            <p>Voir les offres<i class="fa-solid fa-angles-right ms-3" style="color: #ffffff;"></i></p> 
+            
+        </div>
+    </section>
+    <section class="destination text-white d-flex align-items-center justify-content-around col-12">
+        <div class="col-3">
+            <p>destination</p>
+        </div>   
+        <div class="col-5"> 
+            <p>Voir les offres<i class="fa-solid fa-angles-right ms-3" style="color: #ffffff;"></i></p> 
+            
+        </div>
+    </section>
 
 <?php include "./partials/footer.php"?>
 
