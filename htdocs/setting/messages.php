5f2da4b46e0e4ed37582fb0e41caa82931479d8a
lucas dechavanne <lucasdechavanne22@gmail.com>
2024-03-01 14:00:33 +0100

    dossier prets a etre utiliser

---
+++ htdocs/setting/messages.php
@@ -0,0 +1,16 @@
+<?php 
+
+if (!empty($_GET['success'])) {?>
+    <div class="alert alert-success text-center" id="slide" role="alert"><i class="fa-solid opacity-25"></i>
+        <?= $_GET['success'] ?>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+<?php } ?>
+
+<?php if (!empty($_GET['error'])) {?>
+    <div class="alert alert-danger text-center alert-dismissible fade show" id="slide" role="alert"><i class="fa-solid fa-bell fa-shake fa-lg mx-3"></i>
+        <?= $_GET['error'] ?>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+<?php } ?>
+
