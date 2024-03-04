e8bca1ac4106bad5600f61899e8d54467666458f
lucas dechavanne <lucasdechavanne22@gmail.com>
2024-03-04 14:34:58 +0100

    ajout de la page de details

--- htdocs/pages/details.php
+++ htdocs/pages/details.php
@@ -1,8 +1,11 @@
-<?php include "../partials/header.php";?>
+<?php include "../partials/header.php";
+ 
+ $DestinationName = $_GET['Destination'];
+
+?>
 
 <!-- LIEN VERS LA FEIULLE DE STYLE DE LA PAGE  -->
 
 <link rel="stylesheet" href="../style/login.css">
 
-    <!-- formulaire d'inscription  -->
 
