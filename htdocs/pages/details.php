5dad20c0521144eff2e2cd44cab717f3ebadfd1e
StephGarage404 <stephane.1995@hotmail.fr>
2024-03-05 09:26:53 +0100

    hh

--- htdocs/pages/details.php
+++ htdocs/pages/details.php
@@ -21,8 +21,9 @@ foreach ($destinationByOperator as $destination) {
         // var_dump($destination)
     ?>   
         <section class="affichage d-flex align-items-center justify-content-around col-12 mt-5" style="background-image: url(../images/images/destination/<?=$destination['location']?>.jpeg);">
-            <div class="col-3 d-flex justify-content-center align-items-center" style="background-color: rgba(0, 0, 0, 0.5);">
+            <div class="text-white col-6 d-flex justify-content-center align-items-center" style="background-color: rgba(0, 0, 0, 0.5);">
                 <p class="fs-1"><?=$destination['location']?></p>
+                <p class="fs-1">prix :<?=$destination['price']?></p>
             </div>
         </section>
 
