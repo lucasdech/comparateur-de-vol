78d2fe270d1e6ec22d3b5a5c641884ff2c8f82ca
lucas dechavanne <lucasdechavanne22@gmail.com>
2024-03-06 09:30:10 +0100

    push mercredi matin

--- htdocs/process/login_and_register_process/add_user.php
+++ htdocs/process/login_and_register_process/add_user.php
@@ -20,8 +20,6 @@ if (!empty($_POST['Username'])
             $hashed_password,
         ]);
 
-        $_SESSION['id'] = $connexion->lastInsertId();
-
         header('Location: ../../index.php?success= Compte Creer avec succes !');
 }else{
 
